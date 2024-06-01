02cc21006a2766ad5f82a74bca03cbf8addd81d4
Hongning <howang1196@gmail.com>
2024-06-01 15:24:45 -0700

    initial KMeans

---
+++ kmeans.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+file_path = 'values.csv'
+data = pd.read_csv(file_path, index_col=0) 
+features = data.columns.tolist()
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(data[features])
+
+
+inertia = []
+K = range(1, 11) 
+
+# test for elbow
+for k in K:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(scaled_data)
+    inertia.append(kmeans.inertia_)
+
+plt.figure(figsize=(10, 6))
+plt.plot(K, inertia, markersize=8)
+plt.xlabel('Number of clusters (k)')
+plt.ylabel('Inertia')
+plt.title('Elbow Method For Optimal k')
+plt.savefig('./figures/elbow.jpg')
+plt.close()
+
+
+optimal_k = 4
+
+#kmeans
+kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+clusters = kmeans.fit_predict(scaled_data)
+
+
+data['Cluster'] = clusters
+
+# Display the dataframe with cluster labels
+pca = PCA(n_components=2)
+components = pca.fit_transform(scaled_data)
+
+# PCA Analysis
+pca_df = pd.DataFrame(data=components, columns=['PC1', 'PC2'])
+pca_df['Cluster'] = clusters
+pca_df['Year'] = data.index
+
+plt.figure(figsize=(14, 10)) 
+sns.scatterplot(x='PC1', y='PC2', hue='Cluster', data=pca_df, palette='viridis')
+plt.title('PCA Clusters')
+
+# Annotate each point
+for i in range(pca_df.shape[0]):
+    plt.annotate(pca_df['Year'].iloc[i], (pca_df['PC1'].iloc[i], pca_df['PC2'].iloc[i]), 
+                 fontsize=6)
+plt.savefig('./figures/clusters_annotated.jpg')
+plt.close() 
---
+++ process.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+from pathlib import Path
+# Define the path to the folder containing the CSV files
+folder_path = 'raw'
+
+# Initialize a dictionary to store aggregated data
+data_dict = {year: [] for year in range(1967, 2024)}
+
+def extract_year(date_str):
+    try:
+        date = pd.to_datetime(date_str, errors='coerce')
+        if pd.notnull(date):
+            return date.year
+    except Exception as e:
+        print(f"Error converting date: {date_str}, Error: {e}")
+    return None
+rows = []
+for filename in os.listdir(folder_path):
+    name = Path(filename).stem
+    if filename.endswith('.csv'):
+        file_path = os.path.join(folder_path, filename)
+        df = pd.read_csv(file_path)
+        df['Year'] = df.iloc[:, 0].apply(extract_year)
+
+        df_filtered = df[(df['Year'] >= 1967) & (df['Year'] <= 2023)]
+
+        for year in range(1967, 2024):
+            value = df_filtered[df_filtered['Year'] == year].iloc[:, 1].mean()
+            data_dict[year].append(value)
+    rows.append(name)
+
+
+results_df = pd.DataFrame(data_dict)
+results_df.index = rows
+results_df = results_df.T
+
+results_df.to_csv('values.csv', index = True)
+
+# Display the results
+print(results_df)
